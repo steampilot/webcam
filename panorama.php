f664503647f2211a2610fcec0d1c32b3012a0fc2
Jérôme <github@steampilot.ch>
2015-03-17 11:35:19 +0100

    Tested Working at School

--- panorama.php
+++ panorama.php
@@ -14,29 +14,52 @@ include 'config.php';
 
 
 
-$file = '/snapshots/snapshot_';
 
-header('Location: '.$move_home);
-sleep(1);
-$snapshot1 = file_get_contents($get_snapshot);
-sleep(1);
-header('Location: '.$move_left);
-$data = header('Location: '.$get_snapshot);
-file_put_contents($file.'home',$data);
-//header('Location: '.$get_snapshot)
-sleep(1);
-header('Location: '.$move_left);
-$data = header('Location: '.$get_snapshot);
-file_put_contents($file.'home',$data);
-//header('Location: '.$get_snapshot)
-sleep(1);
-header('Location: '.$move_home);
-sleep(1);
-header('Location: '.$move_right);
-//header('Location: '.$get_snapshot)
-sleep(1);
-header('Location: '.$move_right);
-//header('Location: '.$get_snapshot)
-sleep(1);
-header('Location: '.$move_right);
-//header('Location: '.$get_snapshot)
+
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+$filecontent = file_get_contents('http://10.142.126.189/cgi-bin/video.jpg');
+$filename = 'file.jpg';
+file_put_contents($filename, $filecontent);
+echo '<img src="file.jpg">';
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+$filecontent = file_get_contents('http://10.142.126.189/cgi-bin/video.jpg');
+$filename = 'file1.jpg';
+file_put_contents($filename, $filecontent);
+echo '<img src="file1.jpg">';
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+echo '<img src="http://10.142.126.189/cgi-bin/video.jpg">';
+
+$filecontent = file_get_contents('http://10.142.126.189/cgi-bin/video.jpg');
+$filename = 'file2.jpg';
+file_put_contents($filename, $filecontent);
+echo '<img src="file2.jpg">';
+
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+file_get_contents('http://10.142.126.189/cgi-bin/camctrl.cgi?move=right');
+$filecontent = file_get_contents('http://10.142.126.189/cgi-bin/video.jpg');
+$filename = 'file3.jpg';
+file_put_contents($filename, $filecontent);
+echo '<img src="file3.jpg">';
